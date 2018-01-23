b3d628dceab82399fb68bdc97d1190d2cf675187
David Laskowski <davidlaskowski93@googlemail.com>
2018-01-23 12:21:32 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicSwipeAllModule } from 'ionic-swipe-all';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { CarouselComponent } from "../components/carousel.component";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicSwipeAllModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { CarouselComponent } from "../../components/carousel.component";
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +13,91 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  private slides = [{
+      id: 4,
+      description: 'Ashutosh',
+      color: '#630460',
+      isSelected: false,
+    },
+    {
+      id: 1,
+      description: 'Saina',
+      color: '#0072bc',
+      isSelected: false,
+    },
+    {
+      id: 2,
+      description: 'Sakshi',
+      color: '#39b54a',
+      isSelected: false,
+    },
+    {
+      id: 3,
+      description: 'Sushil',
+      color: '#f26522',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    },
+    {
+      id: 4,
+      description: 'Sindhu',
+      color: '#ed1c24',
+      isSelected: false,
+    }
+  ];
 
 }
